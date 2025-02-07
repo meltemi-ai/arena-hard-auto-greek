c0070d08e7eee65303adc5049289cd56c1e6ac26
Dimitrios Roussis <droussis@outlook.com.gr>
2025-02-08 01:26:09 +0200

    Add generation with SFT Mixed Run1

--- vllm_serve.sh
+++ vllm_serve.sh
@@ -1,13 +1,16 @@
-MODEL_PATH="/home/ubuntu/models/krikri-annealing-dpo-mixed"
-MODEL_NAME="krikri-annealing-dpo-mixed-run1"
-NUM_GPUS=8
+BASE_MODEL_PATH="/opt/dlami/nvme/models"
+MODEL_PATH="$BASE_MODEL_PATH/krikri-annealing-sft-mixed-bf16"
+MODEL_NAME="krikri-annealing-sft-mixed-run1"
+NUM_GPUS=4
 MAX_MODEL_LEN=8192
 
 vllm serve $MODEL_PATH \
   --served-model-name $MODEL_NAME \
   --tensor-parallel-size $NUM_GPUS \
-  --max-model-len $MAX_MODEL_LEN \
   --max-num-batched-tokens $MAX_MODEL_LEN \
   --enforce-eager \
-  --dtype auto \
+  --dtype 'bfloat16' \
+  --gpu_memory_utilization 0.94 \
   --api-key token-abc123
+
+#  --max-model-len $MAX_MODEL_LEN
