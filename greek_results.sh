8a5d8db4d87977eb9c7f8ca807a8390e324ec425
Dimitris Roussis <dimitris.roussis@athenarc.gr>
2025-02-07 18:15:57 +0200

    Add evals with GPT-4o judge

---
+++ greek_results.sh
@@ -0,0 +1,9 @@
+python show_result.py \
+    --judge-name "claude-3.5-sonnet-v2" \
+    --baseline "gpt-4o-mini" \
+    --output
+
+python show_result.py \
+    --judge-name "gpt-4o" \
+    --baseline "gpt-4o-mini" \
+    --output
