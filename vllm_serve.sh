25412710de65f530fc4c99dfd7b041ff6943ac95
Ubuntu <ubuntu@ip-172-31-36-23.us-east-2.compute.internal>
2025-03-17 15:16:35 +0000

    Add gens by Gemma 3

--- vllm_serve.sh
+++ vllm_serve.sh
@@ -50,8 +50,8 @@ BASE_MODEL_PATH="/home/ubuntu/models"
 #MODEL_NAME="krikri-annealing-dpo-max-length-norm-dpo-fixes-length-norm-with-thinking"
 # MODEL_PATH="$BASE_MODEL_PATH/base_neo_arcee_fusion_round-2_base"
 # MODEL_NAME="base_neo_arcee_fusion_round-2_base"
-MODEL_PATH="google/gemma-3-27b-it"
-MODEL_NAME="gemma-3-27b-it"
+MODEL_PATH="google/gemma-3-4b-it"
+MODEL_NAME="gemma-3-4b-it"
 
 NUM_GPUS=8
 MAX_MODEL_LEN=8192
@@ -59,11 +59,19 @@ MAX_MODEL_LEN=8192
 vllm serve $MODEL_PATH \
   --served-model-name $MODEL_NAME \
   --tensor-parallel-size $NUM_GPUS \
-  --enforce-eager \
   --enable-chunked-prefill False \
   --dtype 'bfloat16' \
   --gpu_memory_utilization 0.94 \
   --api-key token-abc123
+
+# vllm serve $MODEL_PATH \
+#  --served-model-name $MODEL_NAME \
+#  --tensor-parallel-size $NUM_GPUS \
+#  --enforce-eager \
+#  --enable-chunked-prefill False \
+#  --dtype 'bfloat16' \
+#  --gpu_memory_utilization 0.94 \
+#  --api-key token-abc123
   
 #  --download-dir $CACHE_DIR
 #  --max-model-len $MAX_MODEL_LEN
